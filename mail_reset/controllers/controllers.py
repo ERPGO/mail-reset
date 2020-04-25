be9391e7d6745bbdc73c57c509632f517fef7f29
Your Name <you@example.com>
2020-04-25 14:00:29 +0000

    change

--- mail_reset/controllers/controllers.py
+++ mail_reset/controllers/controllers.py
@@ -3,8 +3,18 @@ from odoo import http
 
 class MailReset(http.Controller):
     
-#     def is_email_registered(self, email):
-#         if http.request.env['mail_reset.users'].search([('email': email)])
+    def _domain_exists(self, domain):
+        if http.request.env['mail_reset.domain'].search([('name','=', domain)]):
+            return True
+    
+    def _email_registered(self, email):
+        domain = email.split('@')[1]
+        if self._domain_exists(domain):
+            username = http.request.env['mail_reset.users'].search([('email','=', email),('domain','=',domain)])
+            if username:
+                return True
+            else:
+                return False
     
     @http.route('/mail_reset/ask', type='http', auth='public', website=True, csrf=False)
     def reset_form(self, **kw):
@@ -14,8 +24,11 @@ class MailReset(http.Controller):
     @http.route('/mail_reset/submit', methods=['POST'], type='http', auth='public', website=True, csrf=False)
     def reset_form_submit(self, **kw):
         email = kw.get('email')
-        return email
-    
+        if self._email_registered(email):
+            return f"Reset instructions has been sent to your recovery email"
+        else:
+            return f"{email} is not registered"
+            
 #     @http.route('/mail_reset/mail_reset/objects/', auth='public')
 #     def list(self, **kw):
 #         return http.request.render('mail_reset.listing', {
