6c22689f82aa2a79f16e6306df29c695387d114f
jumshud <cumshud@gmail.com>
2020-04-29 18:54:12 +0000

    fix

--- mail_reset/models/mail_reset_domain.py
+++ mail_reset/models/mail_reset_domain.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, requests
+from odoo import models, fields, api
 
 class Mail_Reset_Domain(models.Model):
     _name = 'mail_reset.domain'
