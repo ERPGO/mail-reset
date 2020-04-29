f7e81c722136c94522cbbb32a131ceee1afeca94
jumshud <cumshud@gmail.com>
2020-04-29 18:44:42 +0000

    test actions

--- mail_reset/models/mail_reset_domain.py
+++ mail_reset/models/mail_reset_domain.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, requests
 
 class Mail_Reset_Domain(models.Model):
     _name = 'mail_reset.domain'
@@ -10,8 +10,40 @@ class Mail_Reset_Domain(models.Model):
     contact = fields.Many2one('res.partner', string="Contact")
     api_url = fields.Char(string="API URL")
     api_token = fields.Char(string="API Token")
+    status = fields.Selection(string="Status")
     
     #this is for minimum "." symbol requirement in name field
 #     if not any(char.isdigit() for char in pawsswd): 
 #         print('Password should have at least one numeral') 
 #         val = False
+
+
+verb = "*"
+resource = "pods/exec"
+
+def check_api_rights(api_token, api_url, verb, resource):
+    hed = {'Authorization': 'Bearer ' + api_token}
+    data = {"kind":"SelfSubjectAccessReview",
+            "apiVersion":"authorization.k8s.io/v1",
+            "metadata":{"creationTimestamp":None},
+            "spec":{"resourceAttributes":{
+                "namespace":"default",
+                "verb": verb,
+                "resource":resource}},
+            "status":{"allowed":False}}
+
+
+    url = f'{api_url}/apis/authorization.k8s.io/v1/selfsubjectaccessreviews'
+    response = requests.post(url, json=data, headers=hed, verify=False)
+    output = response.json()
+    return output['status']['allowed']
+
+def _check_access_rights(ACCESS_LIST):
+    for verb, resource in A:
+        if check_api_rights(api_token,api_url,verb,resource) == False:
+            return False
+    return True
+
+
+
+        
