71529284ae670cc2f8db9c0bcc4d369c61096878
Nurlan Farajov <18286626+nurlanf@users.noreply.github.com>
2020-06-25 13:12:49 +0200

    Update __manifest__.py

--- mail_reset/__manifest__.py
+++ mail_reset/__manifest__.py
@@ -25,7 +25,7 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
+#         'security/ir.model.access.csv',
         'views/reset_submit_form.xml',
         'views/reset_users.xml',
         'views/reset_domain.xml',
